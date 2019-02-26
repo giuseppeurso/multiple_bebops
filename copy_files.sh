378dc5dd279c56d8659094fcdaf8f8ff49117b3c
giuseppeurso <giuseppe.urso1@gmail.com>
2019-02-27 00:34:12 +0100

    Update copy_files.sh
    remove files from copying that are already there

--- copy_files.sh
+++ copy_files.sh
@@ -1,13 +1,4 @@
 adb shell mount -o remount,rw / &&
-adb push libiw.so.29 /lib/libiw.so.29 &&
-adb push libiw.so  /lib/libiw.so &&
-adb push ifrename /sbin/ifrename &&
-adb push iwconfig /sbin/iwconfig &&
-adb push iwevent /sbin/iwevent &&
-adb push iwgetid /sbin/iwgetid &&
-adb push iwlist /sbin/iwlist &&
-adb push iwpriv /sbin/iwpriv &&
-adb push iwspy /sbin/iwspy &&
 adb push wpa_cli /bin/wpa_cli &&
 adb push wpa_passphrase /bin/wpa_passphrase &&
 adb push wpa_supplicant /bin/wpa_supplicant &&
